4f67f605e1d90d1023178194a7ea8ac7b68c1bb0
Nikolaj Skovmose <niko480a@student.eadania.dk>
2017-09-01 10:41:38 +0200

    all

---
+++ BudgetManagerV2/Controllers/CategoriesController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BudgetManagerV2.Models;
+
+namespace BudgetManagerV2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private BudgetManagerEntities db = new BudgetManagerEntities();
+
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var category = db.Category.Include(c => c.Category2);
+            return View(category.ToList());
+        }
+
+        // GET: Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.Cat_ID = new SelectList(db.Category, "Id", "Name");
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Cat_ID")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Category.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Cat_ID = new SelectList(db.Category, "Id", "Name", category.Cat_ID);
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Cat_ID = new SelectList(db.Category, "Id", "Name", category.Cat_ID);
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Cat_ID")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Cat_ID = new SelectList(db.Category, "Id", "Name", category.Cat_ID);
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Category.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Category.Find(id);
+            if (!(category.Category1.Count > 0))
+            {
+
+                db.Category.Remove(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "This Category is currently in use by a subcategory";
+
+            }
+
+            return View("Index", db.Category.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
